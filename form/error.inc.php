57ef11d3fca384f2fc162e2632ca2ddc31f5fa50
Cristian Yarce <cristianyarcer@gmail.com>
2021-05-01 11:30:41 -0400

    first commit

---
+++ form/error.inc.php
@@ -0,0 +1,138 @@
+<style type="text/css">
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-family: "Lato", sans-serif;
+
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  padding: 10px 0;
+  text-align: center;
+}
+
+/*=====================
+
+  Header
+
+=========================*/
+
+
+
+header {
+  text-align: center;
+  background: #333;
+  padding: 20px 0;
+}
+
+header nav ul li {
+  display: inline;
+}
+
+header nav a {
+  color: white;
+  font-size: 24px;
+  text-decoration: none;
+  margin: 0 20px;
+}
+
+header nav a:hover{
+  color: #2caae2;
+}
+
+header nav a:active{
+  color: #1072ba;
+}
+
+.containercontact {
+  text-align: center;
+  padding: 80px 0;
+}
+
+.containerfoot{
+  width: 900px;
+  margin: 20px 310px;
+}
+
+.location,
+.phone,
+.socialm{
+  background: #333;
+  text-align: center;
+  width: 275px;
+  height: 170px;
+  float: left;
+}
+
+
+footer {
+    color: white;
+}
+
+</style>
+
+
+<header>
+
+  <div class="logo-container">
+    <a href="index.html">
+      <img src="images/logo.png" alt="Cristian Yarce" width="300"  />
+    </a>
+  </div>
+
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="aboutus.html">About Us</a></li>
+      <li><a href="services.html">Services</a></li>
+      <li><a href="contact.html">Contact Us</a></li>
+    </ul>
+  </nav>
+</header>
+
+<div class="containercontact">
+
+	<h1>Missing fields</h1>
+	<p>Sorry, you have not completed all of the required fields.</p>
+	<p>Please hit <a href="#" onClick="history.go(-1)">back</a> and complete the following required fields.</p>
+
+	<ul>
+	<?php
+		for($i=0; $i<count($this->missing_required_fields); $i++){
+			echo "<li>" . $this->missing_required_fields[$i]['title'] . "</li>\n";
+		}
+	?>
+	</ul>
+
+	<p><strong><a href="#" onClick="history.go(-1)">Back to form</a></strong></p>
+</div>
+
+<footer>
+  <div class="containerfoot">
+    <div class="location">
+
+      <p>Cristian Yarce<br>
+      123 E Bay Blvd, Suite 100<br>
+      Tampa, Florida</p>
+    </div>
+    <div class="phone">
+      <br>
+      <p>Phone: (754- 333 -2211)
+      <br><br><br><br> Design by: Cristian Yarce</p>
+    </div>
+    <div class="socialm">
+      <p>Stay Connected</p>
+      <br>
+      <a href="https://facebook.com" target="_blank"><img src="images/fb.png" alt="Facebook" width="25" /></a>
+      <a href="https://instagram.com" target="_blank"><img src="images/ig.png" alt="Instagram" width="25" /></a>
+      <a href="https://youtube.com" target="_blank"><img src="images/yt.png" alt="YouTube" width="25" /></a>
+    </div>
+  </div>
+</footer>
---
+++ form/thankyou.inc.php
@@ -0,0 +1,123 @@
+<style type="text/css">
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-family: "Lato", sans-serif;
+
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  padding: 10px 0;
+  text-align: center;
+}
+
+/*=====================
+
+  Header
+
+=========================*/
+
+
+
+header {
+  text-align: center;
+  background: #333;
+  padding: 20px 0;
+}
+
+header nav ul li {
+  display: inline;
+}
+
+header nav a {
+  color: white;
+  font-size: 24px;
+  text-decoration: none;
+  margin: 0 20px;
+}
+
+header nav a:hover{
+  color: #2caae2;
+}
+
+header nav a:active{
+  color: #1072ba;
+}
+
+.containercontact {
+  text-align: center;
+  padding: 80px 0;
+}
+
+.containerfoot{
+  width: 900px;
+  margin: 20px 310px;
+}
+
+.location,
+.phone,
+.socialm{
+  background: #333;
+  text-align: center;
+  width: 275px;
+  height: 170px;
+  float: left;
+}
+footer {
+    color: white;
+}
+
+</style>
+
+<header>
+
+  <div class="logo-container">
+    <a href="index.html">
+      <img src="images/logo.png" alt="Cristian Yarce" width="300"  />
+    </a>
+  </div>
+
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="aboutus.html">About Us</a></li>
+      <li><a href="services.html">Services</a></li>
+      <li><a href="contact.html">Contact Us</a></li>
+    </ul>
+  </nav>
+</header>
+
+<div class="containercontact">
+  <h1>Thank you for your enquiry</h1>
+  <br>
+  <p>Thanks for your interest. we have received your message, and we will be in touch shortly!</p>
+</div>
+
+<footer>
+  <div class="containerfoot">
+    <div class="location">
+
+      <p>Cristian Yarce<br>
+      123 E Bay Blvd, Suite 100<br>
+      Tampa, Florida</p>
+    </div>
+    <div class="phone">
+      <br>
+      <p>Phone: (754- 333 -2211)
+      <br><br><br><br> Design by: Cristian Yarce</p>
+    </div>
+    <div class="socialm">
+      <p>Stay Connected</p>
+      <br>
+      <a href="https://facebook.com" target="_blank"><img src="images/fb.png" alt="Facebook" width="25" /></a>
+      <a href="https://instagram.com" target="_blank"><img src="images/ig.png" alt="Instagram" width="25" /></a>
+      <a href="https://youtube.com" target="_blank"><img src="images/yt.png" alt="YouTube" width="25" /></a>
+    </div>
+  </div>
+</footer>
